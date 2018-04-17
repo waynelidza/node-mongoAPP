767ab52631de8b34719f612c0ff228130fde8add
waynelidza <leewaynemaponga@gmail.com>
2018-04-17 15:03:41 +0200

    added logging again

--- index.js
+++ index.js
@@ -12,42 +12,26 @@ app.use(routes);
 //connect to mongodb
 mongoose.connect("mongodb://localhost/ivy");
 mongoose.Promise = global.Promise;
-app.use(expressWinston.logger({
-    transports: [
-        new winston.transports.Console({
-            json: true,
-            colorize: true
-        })
-    ],
-    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
-    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
-    ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
-}));
-app.use(expressWinston.errorLogger({
-    transports: [
-        new winston.transports.Console({
-            json: true,
-            colorize: true
-        })
-    ]
-}));
+
+
 //error handling
 app.use(function (err,req,res,next) {
     console.log('err');
-
+    winston.add(winston.transports.File, { filename: 'somefile.log' });
 
 
     console.log(err.message);
     if(err.message.search('duplicate')){
         res.status(409).send({message:'Phonenumber exist'});
+
     }else{
         res.status(422).send({error:err.message});
+
     }
 });
 
 app.listen(process.env.port||3000,function () {
+
     console.log("now listening");
 
 });
