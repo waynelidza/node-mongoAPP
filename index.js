7d0023fca8a6f5dfa5b70b3a3fef180426db1b43
waynelidza <leewaynemaponga@gmail.com>
2018-04-17 14:51:26 +0200

    added logging again

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const bodyPaser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 const routes = require('./routes/api');
-
+var winston = require('winston');
+expressWinston = require('express-winston');
 app.use(bodyPaser.json());
 //init routes
 app.use(routes);
@@ -11,8 +12,27 @@ app.use(routes);
 //connect to mongodb
 mongoose.connect("mongodb://localhost/ivy");
 mongoose.Promise = global.Promise;
-
-
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        })
+    ],
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+}));
+app.use(expressWinston.errorLogger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        })
+    ]
+}));
 //error handling
 app.use(function (err,req,res,next) {
     console.log('err');
